81357da9707eae08ed592e095ff9d61601480e64
Omar Houmz <omar.houmz@schibsted.com>
2019-06-28 17:18:26 +0100

    remove hello rectangle widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,46 +3,9 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:hello_rectangl/unit_converter_app.dart';
 
-import 'category.dart';
+import 'package:hello_rectangl/unit_converter_app.dart';
 
 void main() {
   runApp(UnitConverterApp());
-}
-
-const _categoryName = 'Check Here';
-const _categoryIcon = Icons.check_circle;
-const _categoryColor = Colors.indigo;
-
-class HelloRectangle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.indigo[50],
-        height: 400.0,
-        child: Column(
-          children: <Widget>[
-            Container(
-              color: Colors.greenAccent,
-              height: 120.0,
-              child: Center(
-                child: Text(
-                  'Hello',
-                  style: TextStyle(fontSize: 40.0),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            Category(
-              name: _categoryName,
-              color: _categoryColor[200],
-              iconLocation: _categoryIcon,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
